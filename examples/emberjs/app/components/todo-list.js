37840991aff8af1d2d89ff061d808c4d38ceb650
Christian Wesselhoeft <hi@xtian.us>
2015-06-30 16:13:25 -0400

    Add Ember CLI example.

---
+++ examples/emberjs/app/components/todo-list.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	classNames: ['todo-list'],
+	tagName: 'ul',
+
+	actions: {
+		cancelEditing(todo) {
+			todo.set('isEditing', false);
+			todo.rollbackAttributes();
+		},
+
+		deleteTodo(todo) {
+			todo.deleteRecord();
+			todo.save();
+		},
+
+		editTodo(todo) {
+			todo.set('isEditing', true);
+
+			Ember.run.schedule('afterRender', () => {
+				this.$('.edit').trigger('focus');
+			});
+		},
+
+		toggleTodo(todo) {
+			todo.toggleProperty('isCompleted');
+			todo.save();
+		},
+
+		updateTodo(todo) {
+			const title = todo.get('title').trim();
+
+			if (title) {
+				todo.set('title', title);
+				todo.set('isEditing', false);
+				todo.save();
+			} else {
+				this.send('cancelEditing', todo);
+			}
+		}
+	}
+});
